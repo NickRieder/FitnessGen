82c168d0fa9723641381013c9da4dd3611891b80
Gabe <kornickg@gmail.com>
2022-02-09 21:22:51 -0500

    Set up some routing and started dev on Home Component

---
+++ fitness-generator/src/SignIn.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const SignIn = () => {
+
+    return (
+        <div id="SignIn">
+            <h1>Coming soon...</h1>
+        </div>
+    )
+}
+export default SignIn;
