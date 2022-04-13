b087624e2914e6e2a78fc6ea7ac0c8b3acd5cbc1
Gabe <kornickg@gmail.com>
2022-04-13 16:13:28 -0400

    Workout View video fetching in progress

--- fitness-generator/src/WorkoutsView/DailyWorkout.js
+++ fitness-generator/src/WorkoutsView/DailyWorkout.js
@@ -3,12 +3,30 @@ import Form from 'react-bootstrap/Form';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 import { Row, Col, Container } from 'react-bootstrap';
-import { getWorkout, getUserInfo, AuthContext } from './../config/firebase'
+import { doc, getDoc } from "firebase/firestore"
+import { db, AuthContext, getUserInfo, getWorkout } from './../config/firebase'
 import "./DailyWorkout.css"
 
 const DailyWorkout = ({leg, back, chest, arms, core}) => {
+    /*const [legs, setLeg] = useState(null)
+    const[vid, setVid] = useState(null)*/
 
-  
+   /* const fetchVideo = async (workout) =>{
+        
+        const docRef = doc(db, "Videos", workout);
+        const docSnap = await getDoc(docRef);
+        const result = docSnap.data();
+        return result;
+        
+    }*/
+
+   /* useEffect(()=>{
+        setLeg(leg[(Math.floor(Math.random() * Object.keys(leg).length))])
+        fetchVideo(legs)
+    
+    })
+  */
+
     return (      
         <div className="DailyWorkout" style={{color: "black"}}>
             <Container className="Container">
@@ -27,7 +45,7 @@ const DailyWorkout = ({leg, back, chest, arms, core}) => {
                         <Col className="Col">{leg[(Math.floor(Math.random() * Object.keys(leg).length))]}</Col>
                         <Col className="Col">3</Col>
                         <Col className="Col">10</Col>
-                        <Col className="Col">Video</Col>
+                        <Col className="Col">{}</Col>
                     </Row>
                 </div>
                 {/* Back Workout */}
@@ -57,7 +75,7 @@ const DailyWorkout = ({leg, back, chest, arms, core}) => {
                         <Col>{arms[(Math.floor(Math.random() * Object.keys(arms).length))]}</Col>
                         <Col>3</Col>
                         <Col>10</Col>
-                        <Col>Video</Col>
+                        <Col>{}</Col>
                     </Row>
                 </div>
                 {/* Core Workout */}
